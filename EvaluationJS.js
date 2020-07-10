b385f13b198af8507f65aa47a9589f0931a23434
Valentin Lengaigne <valentin.lengaigne@gmail.com>
2020-07-10 12:30:04 +0200

    ajout des pages pour l'évaluation
    Création des pages pour l'évaluation JS, et mise en place de l'architecture

---
+++ EvaluationJS.js
@@ -0,0 +1,31 @@
+var bouton1 = document.getElementById("Exo1");
+bouton1.addEventListener("click", Exo1);
+
+function Exo1()
+{
+    window.alert("Yeah ! 1");
+}
+
+var bouton2 = document.getElementById("Exo2");
+bouton2.addEventListener("click", Exo2);
+
+function Exo2()
+{
+    window.alert("Yeah ! 2");
+}
+
+var bouton3 = document.getElementById("Exo3");
+bouton3.addEventListener("click", Exo3);
+
+function Exo3()
+{
+    window.alert("Yeah ! 3");
+}
+
+var bouton4 = document.getElementById("Exo4");
+bouton4.addEventListener("click", Exo4);
+
+function Exo4()
+{
+    window.alert("Yeah ! 4");
+}
