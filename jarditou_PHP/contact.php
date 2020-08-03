6d617a799bc75ecca20f62bbdc9e481c237ebf89
Valentin Lengaigne <valentin.lengaigne@gmail.com>
2020-08-03 16:46:25 +0200

    formulaire jarditou en php fait
    Formulaire de jarditou en php terminé, plus qu'à commenter tout ça à tete reposée

--- jarditou_PHP/contact.php
+++ jarditou_PHP/contact.php
@@ -2,135 +2,163 @@
     $Titre = "Contact";
     include("header.php");
 ?>
-<!-- Partie article de la page -->
-<!-- Zone de formulaire -->
-<!-- novalidate désactive la validation du coté HTML pour permettre de la faire côté JavaScript -->
-<form action="script.php" method="POST" id="contact" name="contact" novalidate>
-    <p>* Ces zones sont oligatoires</p>
-    <h1>Vos coordonnées</h1>
-    <!-- Champ pour le nom -->
-    <div class="form-group">
-        <label for="nom">Nom*</label>
-        <input type="text" class="form-control" name="nom" id="nom" placeholder="Veuillez saisir votre nom" required>
-        <!-- texte qui s'affiche si le nom est validé par la vérif -->
-        <div class="valid-feedback">C'est validé mais en vrai j'ai pas trouvé l'expression régulière qui prend en compte les particules, les noms composés et les O' anglophones en même temps donc le champ accepte meme les chiffres...</div>
-        <!-- texte qui s'affiche si le nom n'est pas validé par la vérif -->
-        <div class="invalid-feedback">Entrez un nom valide.</div>
-    </div>
-    <!-- Champ pour le prénom -->
-    <div class="form-group">
-        <label for="prenom">Prénom*</label>
-        <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Veuillez saisir votre prénom" required>
-        <!-- texte qui s'affiche si le prénom est validé par la vérif -->
-        <div class="valid-feedback">C'est validé mais même problème qu'avec Nom, peu importe l'expression régulière, j'arrivais pas à prendre en compte un prénom composé...</div>
-        <!-- texte qui s'affiche si le prénom n'est pas validé par la vérif -->
-        <div class="invalid-feedback">Entrez un prénom valide.</div>
-    </div>
-    <!-- Boutons radio pour le sexe -->
-    <div class="form-group">
-        <label for="sexe">Sexe*</label>
-        <br>
-            <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="sexe" id="feminin" value="feminin" required>
-                <label class="form-check-label" for="feminin"> Féminin</label>
-            </div>
-            <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="sexe" id="masculin" value="masculin" required>
-                <label class="form-check-label" for="masculin"> Masculin</label>
-            </div>
-            <div class="form-check-inline">
-                <input class="form-check-input" type="radio" name="sexe" id="neutre" value="neutre" required>
-                <label class="form-check-label" for="neutre"> Neutre</label>
-                <!-- texte qui s'affiche si un sexe est sélectionné lors de la vérif -->
-                <div class="valid-feedback"></div>
-                <!-- texte qui s'affiche si aucun sexe n'est sélectionné lors de la vérif -->
-                <div class="invalid-feedback" style="margin-left: 1em;"><br>Selectionnez un sexe.</div>
-            </div>
-    </div>
-    <!-- Champ pour la date de naissance -->
-    <div class="form-group">
-        <label for="naissance">Date de Naissance*</label>
-        <div class="input-group">
-            <input type="date" class="form-control" name="naissance" id="naissance" required>
-            <div class="input-group-append">
-                <span class="input-group-text"><i class="fas fa-calendar"></i></span>
-            </div>
-            <!-- texte qui s'affiche si la date naissance est validée par la vérif -->
-            <div class="valid-feedback"></div>
-            <!-- texte qui s'affiche si la date naissance n'est pas validée par la vérif -->
-            <div class="invalid-feedback" id="naissance-invalid">Selectionnez une date.</div>
-        </div>
-    </div>
-    <!-- Champ pour le code postal -->
-    <div class="form-group">
-        <label for="CP">Code Postal*</label>
-        <input type="text" class="form-control" name="CP" id="CP" required>
-        <!-- texte qui s'affiche si le code postal est validé par la vérif -->
-        <div class="valid-feedback"></div>
-        <!-- texte qui s'affiche si le code postal n'est pas validé par la vérif -->
-        <div class="invalid-feedback">Entrez un Code Postal valide.</div>
-    </div>
-    <!-- Champ pour l'adresse -->
-    <div class="form-group">
-        <label for="adresse">Adresse</label>
-        <input type="text" class="form-control" name="adresse" id="adresse">
-    </div>
-    <!-- Champ pour la ville -->
-    <div class="form-group">
-        <label for="ville">Ville</label>
-        <input type="text" class="form-control" name="ville" id="ville">
-    </div>
-    <!-- Champ pour le mail -->
-    <div class="form-group">
-        <label for="email">Email*</label>
-        <input type="email" class="form-control" name="email" id="email" placeholder="dave.loper@afpa.fr" pattern="[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})" required>
-        <!-- texte qui s'affiche si le mail est validé par la vérif -->
-        <div class="valid-feedback"></div>
-        <!-- texte qui s'affiche si le mail n'est pas validé par la vérif -->
-        <div class="invalid-feedback">Entrez une adresse e-mail valide.</div>
-    </div>
-    <h1>Votre demande</h1>
-    <!-- Champ de selection du sujet -->
-    <div class="form-group">
-        <label for="sujet">Sujet*</label>
-        <select class="form-control" name="sujet" id="sujet" required>
-            <option value="" selected disabled>Veuillez sélectionner un sujet</option>
-            <option value="commandes">Mes commandes</option>
-            <option value="question">Question sur un produit</option>
-            <option value="reclamation">Réclamation</option>
-            <option value="autres">Autres</option>
-        </select>
-        <!-- texte qui s'affiche si un sujet est sélectionné lors de la vérif -->
-        <div class="valid-feedback"></div>
-        <!-- texte qui s'affiche si un sujet n'est pas sélectionné lors de la vérif -->
-        <div class="invalid-feedback">Selectionnez un sujet pour votre demande.</div>
-    </div>
-    <!-- Champ d'écriture libre pour la question -->
-    <div class="form-group">
-        <label for="question">Votre question* :</label>
-        <textarea class="form-control" name="question" id="question" rows="2" required></textarea>
-        <!-- texte qui s'affiche si un texte est écrit lors de la vérif -->
-        <div class="valid-feedback">En vrai ici, ça vérifie juste qu'il y a au moins un caractère, donc ça fonctionne même si la personne entre un espace, pas idéal mais bon</div>
-        <!-- texte qui s'affiche si rien n'est écrit lors de la vérif -->
-        <div class="invalid-feedback">Formulez votre demande en quelques mots s'il vous plait.</div>
-    </div>
-    <!-- Case à cocher pour accepter les conditions d'utilisation du traitement des données -->
-    <div class="form-group form-check">
-        <input class="form-check-input" type="checkbox" name="accepted" id="accepted" value=true required>
-        <label class="form-check-label" for="accepted">J'accepte le traitement informatique de ce formulaire.</label>
-        <!-- texte qui s'affiche si la case est cochée lors de la vérif -->
-        <div class="valid-feedback"></div>
-        <!-- texte qui s'affiche si la case n'est pas cochée lors de la vérif -->
-        <div class="invalid-feedback">Cocher cette case est obligatoire.</div>
-    </div>
-    <!-- Boutons du formulaire -->
-    <div class="form-group">
-        <!-- bouton submit du formulaire -->
-        <input class="btn btn-primary bg-dark" type="submit" value="Envoyer">
-        <!-- bouton qui remet à zéro tout les input du formulaire -->
-        <input class="btn btn-primary bg-dark" type="reset" value="Annuler">
-    </div>
-</form>
+<?php
+    $nom = $prenom = $sexe = $naissance = $CP = $adresse = $ville = $email = $sujet = $question = "";
+    $accepted = false;
+
+    $nomval = $prenomval = $sexeval = $naissanceval = $CPval = $emailval = $sujetval = $questionval = $acceptedval = -1;
+
+    $fem = $mas = $neu = false;
+    $sj1 = true;
+    $sj2 = $sj3 = $sj4 = $sj5 = false;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $nomval = $prenomval = $sexeval = $naissanceval = $CPval = $emailval = $sujetval = $questionval = $acceptedval = 1;
+
+        if (empty($_POST["nom"]))
+        {
+            $nomval = 0;
+        }
+        else
+        {
+            $nom = $_POST["nom"];
+        }
+        
+        if (empty($_POST["prenom"]))
+        {
+            $prenomval = 0;
+        }
+        else
+        {
+            $prenom = $_POST["prenom"];
+        }
+
+        if (empty($_POST["sexe"]))
+        {
+            $sexeval = 0;
+        }
+        else
+        {
+            $sexe = $_POST["sexe"];
+            switch ($sexe)
+            {
+                case "feminin":
+                    $fem = true;
+                break;
+
+                case "masculin":
+                    $mas = true;
+                break;
+
+                default:
+                    $neu = true;
+            }
+        }
+
+        if (empty($_POST["naissance"]))
+        {
+            $naissanceval = -2;
+        }
+        else
+        {
+            $naissance = date_create_from_format("Y-m-d", $_POST["naissance"]);
+            $dfiff = date_diff(date_create(), $naissance);
+            if (intval($dfiff->format("%R%a")) >= 0)
+            {
+                $naissanceval = 0;
+            }
+        }
+        
+        if (empty($_POST["CP"]))
+        {
+            $CPval = 0;
+        }
+        else
+        {
+            $CP = $_POST["CP"];
+            if (strspn($CP, "0123456789") != 5 )
+            {
+                $CPval = 0;
+            }
+        }
+        
+        $adresse = $_POST["adresse"];
+        $ville = $_POST["ville"];
+
+        if (empty($_POST["email"]))
+        {
+            $emailval = 0;
+        }
+        else
+        {
+            $email = $_POST["email"];
+            if (preg_match('/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/', $email) == false)
+            {
+                $emailval = 0;
+            }
+        }
+        
+        if (empty($_POST["sujet"]))
+        {
+            $sujetval = 0;
+        }
+        else
+        {
+            $sujet = $_POST["sujet"];
+            $sj1 = false;
+            switch ($sujet)
+            {
+                case "commandes":
+                    $sj2 = true;
+                break;
+
+                case "question":
+                    $sj3 = true;
+                break;
+
+                case "reclamation":
+                    $sj4 = true;
+                break;
+
+                case "autres":
+                    $sj5 = true;
+                break;
+
+                default:
+                    $sj1 = false;
+            }
+        }
+
+        if (empty($_POST["question"]))
+        {
+            $questionval = 0;
+        }
+        else
+        {
+            $question = $_POST["question"];
+        }
+
+        if (empty($_POST["accepted"]))
+        {
+            $acceptedval = 0;
+        }
+        else
+        {
+            $accepted = $_POST["accepted"];
+        }
+    }
+?>
+<?php
+    if ($nomval == 1 && $prenomval == 1 && $sexeval == 1 && $naissanceval == 1 && $CPval == 1 && $emailval == 1 && $sujetval == 1 && $questionval == 1 && $acceptedval == 1)
+    {
+        include("script.php");
+    }
+    else
+    {
+        include("form.php");
+    }
+?>
 <?php
     include("footer.php");
 ?>
--- jarditou_PHP/script.php
+++ jarditou_PHP/script.php
@@ -1,126 +1,64 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vérification du Formulaire</title>
-</head>
-<body>
-    <?php
-        //check
-        $check = true;
-        $errors = array();
-
-        //Nom
-        if (empty($_POST["nom"]))
+<!-- Partie article de la page -->
+<h1>Le formulaire est valide !!!</h1>
+<p>
+    Nom : <?php
+        echo $nom;
+    ?>
+</p>
+<p>
+    Prénom : <?php
+        echo $prenom;
+    ?>
+</p>
+<p>
+    Sexe : <?php
+        echo $sexe;
+    ?>
+</p>
+<p>
+    Date de Naissance : <?php
+        echo date_format($naissance, "j F Y");
+    ?>
+</p>
+<p>
+    Code Postal : <?php
+        echo substr($CP, 0, 2)." ".substr($CP, 2);
+    ?>
+</p>
+<?php
+    if ($adresse != "")
+    {
+        echo "<p>Adresse : ".$adresse."</p>";
+    }
+    if ($ville != "")
+    {
+        echo "<p>Ville : ".$ville."</p>";
+    }
+?>
+<p>
+    Email : <?php
+        echo $email;
+    ?>
+</p>
+<p>
+    Sujet : <?php
+        echo $sujet;
+    ?>
+</p>
+<p>
+    Votre Question : <?php
+        echo $question;
+    ?>
+</p>
+<p>
+    J'accepte : <?php
+        if ($accepted == true)
         {
-            $errors[] = "Entrez un nom valide.";
-        }
-
-        //Prénom
-        if (empty($_POST["prenom"]))
-        {
-            $errors[] = "Entrez un prénom valide.";
-        }
-
-        //Sexe
-        if (empty($_POST["sexe"]))
-        {
-            $errors[] = "Selectionnez un sexe.";
-        }
-
-        //Date de Naissance
-        if (empty($_POST["naissance"]))
-        {
-            $errors[] = "Selectionnez une date.";
-        }
-        else
-        {
-            //echo var_dump($_POST["naissance"]);
-            $naissance = date_create_from_format("Y-m-d", $_POST["naissance"]);
-            $dfiff = date_diff(date_create(), $naissance);
-            if (intval($dfiff->format("%R%a")) >= 0)
-            {
-                $errors[] = "A part si vous êtes né dans le futur, selectionnez une date passée ;-)";
-            }
-        }
-
-        //Code Postal
-        if (empty($_POST["CP"]))
-        {
-            $errors[] = "Entrez un Code Postal valide";
-        }
-        else
-        {
-            if (strspn($_POST["CP"], "0123456789") != 5 )
-            {
-                $errors[] = "Entrez un Code Postal valide";
-            }
-        }
-
-        //Mail
-        if (empty($_POST["email"]))
-        {
-            $errors[] = "Entrez une adresse e-mail valide.";
+            echo "OUI !!!";
         }
         else
         {
-            if (preg_match('/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/', $_POST["email"]) == false)
-            {
-                $errors[] = "Entrez une adresse e-mail valide.";
-            }
-        }
-
-        //Sujet
-        if (empty($_POST["sujet"]))
-        {
-            $errors[] = "Selectionnez un sujet pour votre demande.";
-        }
-
-        //Question
-        if (empty($_POST["question"]))
-        {
-            $errors[] = "Formulez votre demande en quelques mots s'il vous plait.";
-        }
-
-        //J'accepte
-        if (empty($_POST["accepted"]))
-        {
-            $errors[] = "Cocher la case \"J'accepte\" est obligatoire.";
-        }
-
-        //test check
-        if (count($errors) == 0)
-        {
-            //Affichage form
-            echo "<h1>Le formulaire est valide !!!</h1>";
-            /* foreach ($_POST as $champ => $contenu)
-            {
-                echo "<p>".$champ." : ".$contenu."</p>";
-            } */
-            echo "<p>Nom : ".$_POST["nom"]."</p>";
-            echo "<p>Prénom : ".$_POST["prenom"]."</p>";
-            echo "<p>Sexe : ".$_POST["sexe"]."</p>";
-            echo "<p>Date de Naissance : ".date_format($naissance, "j F Y")."</p>";
-            echo "<p>Code Postal : ".substr($_POST["CP"],0,2)." ".substr($_POST["CP"],2)."</p>";
-            if (empty($_POST["adresse"]) != true) { echo "<p>Adresse : ".$_POST["adresse"]."</p>"; }
-            if (empty($_POST["ville"]) != true) { echo "<p>Ville : ".$_POST["ville"]."</p>"; }
-            echo "<p>Email : ".$_POST["email"]."</p>";
-            echo "<p>Sujet : ".$_POST["sujet"]."</p>";
-            echo "<p>Votre Question : ".$_POST["question"]."</p>";
-            echo "<p>J'accepte : ".$_POST["accepted"]."</p>";
-        }
-        else
-        {
-            //Affichage erreurs
-            echo "<h1>Le formulaire n'est pas rempli correctement, il contient les erreurs suivantes :</h1>";
-            echo "<ul>";
-            foreach ($errors as $erreur)
-            {
-                echo "<li>".$erreur."</li>";
-            }
-            echo "</ul>";
+            echo "Normalement c'est impossible d'avoir ce message, normalement...";
         }
     ?>
-</body>
-</html>
+</p>
