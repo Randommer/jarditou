e9528a4f160258fee5b88648df2f1519a089ec4a
Valentin Lengaigne <valentin.lengaigne@gmail.com>
2020-08-04 16:59:52 +0200

    fini detail.php
    J'ai fini detail.php

--- jarditou_PHP/detail.php
+++ jarditou_PHP/detail.php
@@ -3,27 +3,153 @@
     include("header.php");
 ?>
 <!-- Corps du site -->
+<?php
+    require("connexion_bdd.php"); // Inclusion de notrebibliothèque de fonctions
+
+    $db = connexionBase(); // Appel de la fonction deconnexion
+    $pro_id = $_GET["id"];
+    $requete = "SELECT * FROM produits WHERE pro_id=".$pro_id;
+
+    $result = $db->query($requete);
+
+    // Renvoi de l'enregistrement sous forme d'un objet
+    $produit = $result->fetch(PDO::FETCH_OBJ);
+?>
 <div class="row mx-0 mb-1">
-    <?php
-        require("connexion_bdd.php"); // Inclusion de notrebibliothèque de fonctions
-
-        $db = connexionBase(); // Appel de la fonction deconnexion
-        $pro_id = $_GET["id"];
-        $requete = "SELECT * FROM produits WHERE pro_id=".$pro_id;
-
-        $result = $db->query($requete);
-
-        // Renvoi de l'enregistrement sous forme d'un objet
-        $produit = $result->fetch(PDO::FETCH_OBJ);
-    ?>
-
-    <?php echo $produit->pro_libelle; ?> référence <?php echo $produit->pro_ref; ?>
-    <br>
-    <?php echo $produit->pro_description; ?>
-    <br>
-    <?php echo $produit->pro_prix; ?>
-
+    <div class="col-4"></div>
+    <div class="col-4">
+        <?php echo "<img class='img-fluid' src='src/img/".$produit->pro_id.".".$produit->pro_photo."' alt='".$produit->pro_libelle." ".$produit->pro_couleur."'>"; ?>
+    </div>
+    <div class="col-4"></div>
 </div>
+
+<form method="POST">
+
+    <div class="form-group">
+        <label for="ref">Référence :</label>
+        <input type="text" class="form-control" name="ref" id="ref" disabled value="<?php echo $produit->pro_id; ?>">
+    </div>
+
+    <?php
+        $requeteCat = "SELECT cat_id, cat_nom FROM categories ORDER by cat_id";
+
+        $result = $db->query($requeteCat);
+
+        if (!$result)
+        {
+            $tableauErreurs = $db->errorInfo();
+            echo $tableauErreur[2];
+            die("Erreur dans la requête");
+        }
+
+        if ($result->rowCount() == 0)
+        {
+            // Pas d'enregistrement
+            die("La table est vide");
+        }
+
+        $cats = array();
+        $cats[] = "Sélectionnez une catégorie";
+        while ($row = $result->fetch(PDO::FETCH_OBJ))
+        {
+            $cats[] = $row->cat_nom;
+        }
+    ?>
+    <div class="form-group">
+        <label for="cat">Catégorie :</label>
+        <select class="form-control" name="cat" id="cat" disabled>
+            <?php
+                foreach($cats as $i => $kitten)
+                {
+                    echo "<option ";
+                    if ($i == 0)
+                    {
+                        echo "disabled ";
+                    }
+                    if ($produit->pro_cat_id == $i)
+                    {
+                        echo "selected ";
+                    }
+                    echo 'value="'.($i).'">';
+                    echo $kitten;
+                    echo "</option>";
+
+                }
+            ?>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="lib">Libellé :</label>
+        <input type="text" class="form-control" name="lib" id="lib" disabled value="<?php echo $produit->pro_libelle; ?>">
+    </div>
+
+    <div class="form-group">
+        <label for="des">Description :</label>
+        <textarea class="form-control" name="des" id="des" row="2" disabled value=""><?php echo $produit->pro_description; ?></textarea>
+    </div>
+
+    <div class="form-group">
+        <label for="prix">Prix :</label>
+        <input type="text" class="form-control" name="prix" id="prix" disabled value="<?php echo $produit->pro_prix; ?>">
+    </div>
+
+    <div class="form-group">
+        <label for="stock">Stock :</label>
+        <input type="text" class="form-control" name="stock" id="stock" disabled value="<?php echo $produit->pro_stock; ?>">
+    </div>
+
+    <div class="form-group">
+        <label for="color">Couleur :</label>
+        <input type="text" class="form-control" name="color" id="color" disabled value="<?php echo $produit->pro_couleur; ?>">
+    </div>
+
+    <div class="form-group">
+        <label for="block">Produit bloqué ? :</label>
+        <?php
+            if ($produit->pro_bloque == null)
+            {
+                $block = false;
+            }
+            else
+            {
+                $block = true;
+            }
+        ?>
+        <br>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="block" id="blocked" disabled <?php if ($block) { echo "checked"; } ?> value=true>
+                <label class="form-check-label" for="blocked"> Oui</label>
+            </div>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="block" id="unblocked" disabled <?php if (!$block) { echo "checked"; } ?> value=false>
+                <label class="form-check-label" for="unblocked"> Non</label>
+            </div>
+    </div>
+
+    <div class="form-group">
+        <label for="ajout">Date d'ajout :</label>
+        <input type="date" class="form-control" name="ajout" id="ajout" disabled value="<?php echo $produit->pro_d_ajout; ?>">
+    </div>
+
+    <div class="form-group">
+        <label for="modif">Date de modification :</label>
+        <input type="text" class="form-control" name="modif" id="modif" disabled value="<?php echo $produit->pro_d_modif; ?>">
+    </div>
+
+    <div class="form-group">
+        <a href="liste.php" title="retour">
+            <button class="btn btn-secondary " id="retour">Retour</button>
+        </a>
+        <a href="<?php echo 'update_form.php?id='.$pro_id; ?>" title="modifier">
+            <button class="btn btn-warning" id="modifier">Modifier</button>
+        </a>
+        <a href="<?php echo 'delete_form.php?id='.$pro_id; ?>" title="supprimer">
+            <button class="btn btn-danger" id="supprimer">Supprimer</button>
+        </a>
+    </div>
+
+</form>
 <?php
     include("footer.php");
 ?>
