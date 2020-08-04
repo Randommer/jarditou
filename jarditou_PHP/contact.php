12bbfc912c3f9cbfe19ba5a19c8af4efc2fd02a8
Valentin Lengaigne <valentin.lengaigne@gmail.com>
2020-08-04 09:30:36 +0200

    pages php commentees
    Pages du formulaire en PHP commentées et nouvelle mise en page Bootstrap pour la page d'affichage des résultats.

--- jarditou_PHP/contact.php
+++ jarditou_PHP/contact.php
@@ -3,19 +3,24 @@
     include("header.php");
 ?>
 <?php
+    //initialisation des variables qui garderont en mémoire le contenu des champs
     $nom = $prenom = $sexe = $naissance = $CP = $adresse = $ville = $email = $sujet = $question = "";
     $accepted = false;
 
+    //variables sur le statut d'un champ, -1 pas de données POST à prendre en compte, 0 la valeur trouver dans le champ n'est pas valide, 1 le contenu du champ est valide
     $nomval = $prenomval = $sexeval = $naissanceval = $CPval = $emailval = $sujetval = $questionval = $acceptedval = -1;
 
+    //variables à changer en fonction des boutons radio et du select
     $fem = $mas = $neu = false;
     $sj1 = true;
     $sj2 = $sj3 = $sj4 = $sj5 = false;
 
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        //on admet que tout les champs sont valides, puis on test si ils ne le sont pas
         $nomval = $prenomval = $sexeval = $naissanceval = $CPval = $emailval = $sujetval = $questionval = $acceptedval = 1;
 
+        //test pour le champ Nom
         if (empty($_POST["nom"]))
         {
             $nomval = 0;
@@ -25,6 +30,7 @@
             $nom = $_POST["nom"];
         }
         
+        //test pour le champ Prénom
         if (empty($_POST["prenom"]))
         {
             $prenomval = 0;
@@ -34,6 +40,7 @@
             $prenom = $_POST["prenom"];
         }
 
+        //test pour le champ Sexe
         if (empty($_POST["sexe"]))
         {
             $sexeval = 0;
@@ -41,6 +48,7 @@
         else
         {
             $sexe = $_POST["sexe"];
+            //switch pour garder le meme bouton radio sélectionner qu'à envoie précédent
             switch ($sexe)
             {
                 case "feminin":
@@ -56,13 +64,16 @@
             }
         }
 
+        //test du champ Date de Naissance
         if (empty($_POST["naissance"]))
         {
             $naissanceval = -2;
         }
         else
         {
+            //enregitrement de la Date de Naissance dans une variable de type date
             $naissance = date_create_from_format("Y-m-d", $_POST["naissance"]);
+            //test si la date n'est pas du futur
             $dfiff = date_diff(date_create(), $naissance);
             if (intval($dfiff->format("%R%a")) >= 0)
             {
@@ -70,6 +81,7 @@
             }
         }
         
+        //test du champ Code Postal
         if (empty($_POST["CP"]))
         {
             $CPval = 0;
@@ -77,6 +89,7 @@
         else
         {
             $CP = $_POST["CP"];
+            //test si le Code Postal est bien composé de 5 chiffres
             if (strspn($CP, "0123456789") != 5 )
             {
                 $CPval = 0;
@@ -86,6 +99,7 @@
         $adresse = $_POST["adresse"];
         $ville = $_POST["ville"];
 
+        //test du champ Email
         if (empty($_POST["email"]))
         {
             $emailval = 0;
@@ -93,12 +107,14 @@
         else
         {
             $email = $_POST["email"];
+            //test si l'Email est correspond à l'expression régulière d'un mail
             if (preg_match('/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/', $email) == false)
             {
                 $emailval = 0;
             }
         }
         
+        //test du champ Sujet
         if (empty($_POST["sujet"]))
         {
             $sujetval = 0;
@@ -106,6 +122,7 @@
         else
         {
             $sujet = $_POST["sujet"];
+            //switch pour garder le meme select sélectionner qu'à envoie précédent
             $sj1 = false;
             switch ($sujet)
             {
@@ -130,6 +147,7 @@
             }
         }
 
+        //test du champ Votre Question
         if (empty($_POST["question"]))
         {
             $questionval = 0;
@@ -139,6 +157,7 @@
             $question = $_POST["question"];
         }
 
+        //test du champ J'accepte
         if (empty($_POST["accepted"]))
         {
             $acceptedval = 0;
@@ -150,6 +169,7 @@
     }
 ?>
 <?php
+    //test si tout les champs obligatoire sont correctement remplis, si ils le sont, la page affichera les données reçues par le serveurs, sinon elle affichera le formulaire
     if ($nomval == 1 && $prenomval == 1 && $sexeval == 1 && $naissanceval == 1 && $CPval == 1 && $emailval == 1 && $sujetval == 1 && $questionval == 1 && $acceptedval == 1)
     {
         include("script.php");
--- jarditou_PHP/script.php
+++ jarditou_PHP/script.php
@@ -1,64 +1,79 @@
-<!-- Partie article de la page -->
-<h1>Le formulaire est valide !!!</h1>
-<p>
-    Nom : <?php
-        echo $nom;
-    ?>
-</p>
-<p>
-    Prénom : <?php
-        echo $prenom;
-    ?>
-</p>
-<p>
-    Sexe : <?php
-        echo $sexe;
-    ?>
-</p>
-<p>
-    Date de Naissance : <?php
-        echo date_format($naissance, "j F Y");
-    ?>
-</p>
-<p>
-    Code Postal : <?php
-        echo substr($CP, 0, 2)." ".substr($CP, 2);
-    ?>
-</p>
-<?php
-    if ($adresse != "")
-    {
-        echo "<p>Adresse : ".$adresse."</p>";
-    }
-    if ($ville != "")
-    {
-        echo "<p>Ville : ".$ville."</p>";
-    }
-?>
-<p>
-    Email : <?php
-        echo $email;
-    ?>
-</p>
-<p>
-    Sujet : <?php
-        echo $sujet;
-    ?>
-</p>
-<p>
-    Votre Question : <?php
-        echo $question;
-    ?>
-</p>
-<p>
-    J'accepte : <?php
-        if ($accepted == true)
-        {
-            echo "OUI !!!";
-        }
-        else
-        {
-            echo "Normalement c'est impossible d'avoir ce message, normalement...";
-        }
-    ?>
-</p>
+<!-- Corps du site -->
+<div class="row mx-0 mb-1">
+    <!-- Partie article de la page -->
+    <div  class="col-sm-12 mb-2">
+        <h1>Le formulaire est valide !!!</h1>
+        <dl>
+            <dt>Nom : </dt>
+            <dd>
+                <?php echo $nom; ?>
+            </dd>
+
+            <dt>Prénom : </dt>
+            <dd>
+                <?php echo $prenom; ?>
+            </dd>
+
+            <dt>Sexe : </dt>
+            <dd>
+                <?php echo $sexe; ?>
+            </dd>
+
+            <dt>Date de Naissance : </dt>
+            <dd>
+                <?php echo date_format($naissance, "j F Y"); ?>
+            </dd>
+
+            <dt>Code Postal : </dt>
+            <dd>
+                <?php echo substr($CP, 0, 2)." ".substr($CP, 2); ?>
+            </dd>
+
+            <?php
+                if ($adresse != "")
+                {
+                    echo "<dt>Adresse : </dt>";
+                    echo "<dd>";
+                    echo $adresse;
+                    echo "</dd>";
+                }
+                if ($ville != "")
+                {
+                    echo "<dt>Ville : </dt>";
+                    echo "<dd>";
+                    echo $ville;
+                    echo "</dd>";
+                }
+            ?>
+
+            <dt>Email : </dt>
+            <dd>
+                <?php echo $email; ?>
+            </dd>
+
+            <dt>Sujet : </dt>
+            <dd>
+                <?php echo $sujet; ?>
+            </dd>
+
+            <dt>Votre Question : </dt>
+            <dd>
+                <?php echo $question; ?>
+            </dd>
+
+            <dt>J'accepte : </dt>
+            <dd>
+                <?php
+                    if ($accepted == true)
+                    {
+                        echo "OUI !!!";
+                    }
+                    else
+                    {
+                        echo "Normalement c'est impossible d'avoir ce message, normalement...";
+                    }
+                ?>
+            </dd>
+        </dl>
+    </div>
+</div>
